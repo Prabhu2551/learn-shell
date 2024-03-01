fc134fa801bf85953ebb39f1b5bdea64bd0a3db5
prabhu2551 <deva246deva@gmail.com>
2024-03-01 07:58:01 +0530

    expense.frontend

---
+++ sed editor.sh
@@ -0,0 +1,13 @@
+To edit the file context by using sed editor
+
+for example:
+
+search and replace
+delete the context
+adding the lines
+
+sed 's/root/tree' file     (it is changing the output of the file)
+
+-i (it is changing on the file)
+ g (is for the global)
+ -e (is for the entire line).
